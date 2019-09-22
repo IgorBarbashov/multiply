d8ec639fd40b5bdf9837eb9cd626dddf800379bd
= <=>
2019-09-23 02:12:26 +0300

    multiply/feat: add multiplyByDigit function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-module.exports = function multiply(first, second) {
-  // your solution
+// module.exports = function multiply(first, second) {
+function multiply(first, second) {
+  
+  const multiplyByDigit = (number, digit) => {
+    const composition = [...number].reduceRight((acc, el) => {
+      const [result, perenos] = acc;
+      const intermediateResult = el * digit + perenos;
+      const nextResult = `${intermediateResult % 10}${result}`;
+      const nextPerenos = (intermediateResult / 10) ^ 0;
+      return [nextResult, nextPerenos];
+    }, ['', 0]);
+    return composition[1] === 0 ? composition[0] : `${composition[1]}${composition[0]}`;
+  }
+
+  // const arrayForSum = [...first]
+
+  
+  // const sum = (first, second) => {
+  //   const max = first.length >= second.length ? first : second;
+  //   const min = first.length < second.length ? first : second;
+
+
+  // }
+  
+
+
+
+
+
+  const uu = multiplyByDigit(first, parseInt(second));
+  console.log((uu));
+  
+
+
+
 }
+
+multiply('9567670980891220466767', 8);
